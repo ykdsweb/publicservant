bcf800b2bd698e46ce54f67ae6229c6ebe9e2d11
Ozawa <yk.tatsuyaozawa@gmial.com>
2021-03-22 10:42:26 +0900

    firstcommit

---
+++ index.php
@@ -0,0 +1,339 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>倉敷市周辺の公務員のお部屋探しはワイケイ</title>
+  <meta name="keywords" content="岡山,倉敷,公務員,賃貸,賃貸物件,移動,新生活,ひとり暮らし">
+  <meta name="description" content="ワイケイでは、公務員さんのお部屋探しをサポートします。急なお引っ越しにも親切・丁寧・スピーディーに対応します。是非、お気軽にご相談ください。">
+  <!-- Favicon -->
+  <link rel="icon" type="image/png" href="img/favicon_koumuin_00.png">
+  <!-- CSSの読み込み -->
+  <link rel="stylesheet" media="all" href="css/ress.min.css" />
+  <link rel="stylesheet" media="all" href="css/style.css" />
+  <link rel="stylesheet" media="all" href="css/closeBanner.css" />
+  <!-- フォントの読み込み -->
+  <link href="https://fonts.googleapis.com/css?family=M+PLUS+Rounded+1c" rel="stylesheet">
+  <!-- 追加js&jQueryの読み込み -->
+  <script src="js/jquery-2.1.4.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="js/style.js"></script>
+  <script type="text/javascript" src="js/matchheight.js"></script>
+  <script type="text/javascript" src="js/closeBanner.js"></script>
+  <!-- headerに対してアンカーリンクの位置を調整する ここから-->
+  <script>
+    jQuery(function(){
+    var windowWidth = $(window).width();
+    var headerHight = 5000; 
+    jQuery('a[href^=#]').click(function() {
+    var speed = 100;
+    var href= jQuery(this).attr("href");
+    var target = jQuery(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top-headerHight;
+    jQuery('body,html').animate({scrollTop:position}, speed, 'swing');
+    return false;
+    });
+  });
+  </script>
+  <!-- 固定headerに対してアンカーリンクの位置を調整する ここまで-->
+  <!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-T7VKQFP');</script>
+    <!-- End Google Tag Manager -->
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAMFZEiPN3SPwGwINi8_S_6zxZwQPnvNd8"></script>
+	<script type="text/javascript" src="js/matchheight.js"></script>
+    <script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+	
+	  ga('create', 'UA-65743221-42', 'auto');
+	  ga('send', 'pageview');
+	
+	</script>
+
+</head>
+<body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-T7VKQFP"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+<header>    
+<div class="container">
+<div class="row">
+    <div class="col span-12 header">
+        <h1><a href="https://www.chinsta.jp/"><img src="img/YK_1.jpg" width="120" height="" alt="YKロゴ" /></a></h1>
+		<div class="header-box"><a href="https://secure.es-ws.jp/inquiry.html?domain=www.chinsta.jp"><span class="contact-button">お問合せ</span></a></div>
+        </div>
+    </div>
+<div class="row">
+    <div class="col span-12">
+        <nav>
+        <div id="open"><img src="img/button.png"></div>
+        <div id="close"><img src="img/button2.png"></div>    
+            <div id="navi">
+        <ul>
+            <!-- <li><a href="index.html">トップページ</a></li> -->
+            <li><a href="#1">キャンペーンについて</a></li>
+            <li><a href="#2">弊社が選ばれる理由</a></li>
+            <li><a href="#3">お申し込みの流れ</a></li>
+            <!-- <li><a href="#4">アクセス</a></li> -->
+            <li><a href="#5">電話で問合せる</a></li>
+         </ul>
+             </div>
+        </nav>
+    
+        </div>
+    </div>
+    </div>
+</header>
+<section>
+<div class="mainimg">
+  <div class="pc">
+      <img src="img/publicservant_mainimg_PC_02.jpg" alt="メインビジュアル">
+  </div>
+  <div class="sp">
+      <img src="img/publicservant_mainimg_SP_02.jpg" alt="メインビジュアル">
+  </div>
+</div>
+</section>
+<!-- サイドに追尾するバナーを設置する ここから-->
+<section>
+<div class="closeBanner">
+  <a href="https://secure.es-ws.jp/inquiry.html?domain=www.chinsta.jp">
+      <img class="pc" src="img/closeBannerPC_koumuin_01.jpg" alt="キャンペーン開催中">
+  </a>
+  <a href="https://secure.es-ws.jp/inquiry.html?domain=www.chinsta.jp">
+      <img class="sp" src="img/closeBannerSP_koumuin_01.jpg" alt="キャンペーン開催中">
+  </a>
+  <p class="close">
+      <a href="javascript:void(0)">
+          <span>❌</span>
+          <i aria-hidden="true" class="fa fa-times"></i>
+      </a>
+  </p>
+</div>
+</section>
+<!-- サイドに追尾するバナーを設置する ここまで -->
+<main>
+<section>
+  <div class="catch">
+    <h2><span class="bold wf-roundedmplus1c">公務員さん応援キャンペーン2021開催中！！</span></h2>
+    <a class="campaignBanner" href="https://www.chinsta.jp/list.html"><img src="img/SNS_campaign_01.png" width="500" alt="キャンペーンチケット" /></a>
+    <h3>キャンペーン概要</h3>
+    <p>ワイケイでは、公務員さんのお部屋探しをサポート致します。<br /><br />
+    ・2021年春倉敷市周辺でお引っ越しをご検討中の公務員さんが対象です。<br /><br />
+    ・アパマンショップ倉敷沖新店、新倉敷駅前店、倉敷水島店が対象店舗です。<br /><br />
+    ・ご来店いただき、お部屋をご契約いただいた方が対象です。<br /><br />
+    ・こちらのページ、又はSNSのチケットをご提示いただくと仲介手数料が10％OFFになります。<br /><br />
+    是非、お気軽にご相談ください。</p>
+  </div>
+</section>
+<section id="1" class="green_back">
+<h2 class="center"><span class="bold wf-roundedmplus1c">キャンペーンについて</span></h2>
+<div class="container">
+<div class="row flow">
+    <div class="col span-3">
+		<img src="img/subimg_00.jpg" alt="キャンペーン内容00">
+    </div>
+    <div class="col span-9"><h3>公務員さんは仲介手数料<span class="red">10%OFF</span></h3><br /><p>倉敷市周辺の「ネット物件掲載数No1.」のワイケイが理想のお部屋探しをサポート致します。当サイト、またはSNSに掲載のキャンペーンチケットをご提示いただくと、仲介手数料が10%OFF！！</p>
+    </div>
+    </div>	
+<div class="row flow">
+    <div class="col span-3">
+		<img src="img/subimg_01.jpg" alt="キャンペーン内容01">
+    </div>
+    <div class="col span-9"><h3>来店不要!!お家にいながらお部屋を決められる！</h3><br /><p>ご自身のスマホからリモート物件案内も対応可能です。新型コロナウイルスで来店するのがご不安な方も安心です。来店する時間が取れない方も是非ご相談ください。</p>
+    </div>
+    </div>
+<div class="row flow">
+    <div class="col span-3">
+		<img src="img/speed_00.jpg" alt="キャンペーン内容02">
+    </div>
+    <div class="col span-9"><h3>スピーディーな対応！</h3><p>鍵渡しまで最短7日！急なお引っ越しにも親切・丁寧・迅速に対応いたします。是非お気軽にご相談ください。</p>
+    </div>
+    </div>
+	</div>
+
+</section>
+		
+<section id="2">
+        <h2 class="center"><span class="bold wf-roundedmplus1c">弊社が選ばれる理由</span></h2>
+<div class="container center">
+<div class="row">
+    <div class="col span-4">
+		<img src="img/employee_00.jpg" alt="今ならキャンペーン中">
+		<h3>地域密着25年！！</h3>
+		<p>岡山県倉敷市で創業25年の信頼と実績で理想のお部屋探しをサポート致します。</p>
+    </div>
+    <div class="col span-4">
+		<img src="img/okayama_map_00.png" alt="岡山県内の幅広いエリア">
+		<h3>岡山県内の幅広いエリアを網羅</h3>
+		<p>お部屋と職場は近い方が安心です。ご希望のエリアでご要望に合った物件をご案内します。</p>
+    </div>
+    <div class="col span-4">
+		<img src="img/female_02.jpeg" alt="女性スタッフが対応します">
+		<h3>女性スタッフが対応</h3>
+		<p>お部屋探しが初めての女性でも安心！弊社女性スタッフが対応致します。(ご予約の際にご確認ください。)</p>
+    </div>
+    </div>
+	</div>
+</section>
+		
+<section id="3" class="green_back">
+        <h2 class="center"><span class="bold wf-roundedmplus1c">お申し込みの流れ</span></h2>
+<div class="container">
+<div class="row flow">
+    <div class="col span-3">
+		<img src="img/schedule_01.jpg" alt="申し込みについて00">
+    </div>
+    <div class="col span-9"><h3>①お電話/お問合せフォームからご連絡ください</h3><br /><p>先ずはご要望をお伺い致します。ご予算、お引っ越しまでのスケジュール、お部屋の条件、お住まいのエリア、こだわり条件など詳しくお伺い致します。</p>
+    </div>
+    </div>	
+<div class="row flow">
+    <div class="col span-3">
+		<img src="img/Preview_00.jpeg" alt="申し込みについて01">
+    </div>
+    <div class="col span-9"><h3>②お部屋の確認</h3><br /><p>ご条件に合った物件をご紹介致します。来店、又はリモート物件案内にて、お部屋を見て物件を決めて行きます。</p>
+    </div>
+    </div>
+<div class="row flow">
+    <div class="col span-3">
+		<img src="img/housekey_00.jpg" alt="申し込みについて02">
+    </div>
+    <div class="col span-9"><h3>③申込</h3><br /><p>気に入ったお部屋が決まれば申し込み！お手続きについても詳しくご案内致しますので、ご安心ください。</p>
+    </div>
+    </div>
+	</div>
+</section>
+
+<!-- <section id="4">
+        <h2 class="center"><span class="under">アクセス</span></h2>
+<div class="container">
+<div class="row">
+    <div class="col span-12">
+
+    </div>
+    </div>	
+	</div>
+</section> -->
+
+<section id="5">
+<h2 class="center"><span class="bold wf-roundedmplus1c">お問合せ</span></h2>
+<div class="container">
+<!-- <div class="row"> -->
+
+<div class="col span-6">
+  <div class="contact-box">
+    <div class="pc">
+        <a href="tel:086-441-8181"><img class="contactPC" src="img/LP_contactbtnPC_03.jpg" alt="お問合せはこちらから"></p></a>
+    </div>
+    <div class="sp">
+        <a href="tel:086-441-8181"><img class="contactSP" src="img/LP_contactbtnSP_03.jpg" alt="お問合せこちらから"></p></a>
+    </div>
+  </div>
+</div>
+
+    <!-- <div class="col span-6">
+      <a href="te:0120-000-000">
+        <div class="contact-box">
+          <p><img src="img/LP_contactbtn_00.jpg" alt="お電話はこちらから"></p>
+          <p>0120-000-000</p>
+        </div>
+      </a>
+    </div> -->
+	<!-- <div class="col span-6">
+    <a href="">
+      <div class="contact-box">
+        <p><img src="img/mail.png" alt="Eメール"></p>
+        <p>example@mail.com</p>
+      </div>
+    </a>
+    </div>
+    </div> -->
+<div class="row">
+<!-- 今回フォームは不使用 -->
+<div class="col span-12">
+<!-- <form method="post" action="">
+<table class="table full-width">
+  <tbody>
+    <tr>
+      <th><label for="name">お名前</label></th>
+      <td><input class="full-width" type="text" name="お名前" placeholder="名前を記入" id="exampNameInput"></td>
+    </tr>
+	<tr>
+      <th><label for="email">メールアドレス</label></th>
+      <td><input class="full-width" type="email" name="Email" placeholder="メールアドレスを記入" id="exampleEmailInput"></td>
+    </tr>
+    <tr>
+      <th><label for="tel">お電話番号</label></th>
+      <td><input class="full-width" type="tel" name="お電話番号" placeholder="お電話番号を記入" id="exampleTellInput"></td>
+    </tr>
+    <tr>
+      <th><label for="exampleMessage">お問い合わせ内容</label></th>
+      <td><textarea class="full-width textarea" name="お問い合わせ内容" placeholder="用件をご記入ください …" id="exampleMessage"></textarea></td>
+    </tr>
+  </tbody>
+</table>
+<p class="center"><input class="button" type="submit" value="送 信"></p>
+</form>	 -->
+	
+	</div>
+	</div>
+	</div>
+</section>
+<section>
+  <div class="catch">
+    <h2><span class="bold wf-roundedmplus1c">公務員さん応援キャンペーン2021開催中！！</span></h2>
+    <a class="campaignBanner" href="https://www.chinsta.jp/list.html"><img src="img/SNS_campaign_01.png" width="500" alt="キャンペーンチケット" /></a>
+    <h3>キャンペーン概要</h3>
+    <p>ワイケイでは、公務員さんのお部屋探しをサポート致します。<br /><br />
+    ・2021年春倉敷市周辺でお引っ越しをご検討中の公務員さんが対象です。<br /><br />
+    ・アパマンショップ倉敷沖新店、新倉敷駅前店、倉敷水島店が対象店舗です。<br /><br />
+    ・ご来店いただき、お部屋をご契約いただいた方が対象です。<br /><br />
+    ・こちらのページ、又はSNSのチケットをご提示いただくと仲介手数料が10％OFFになります。<br /><br />
+    是非、お気軽にご相談ください。</p>
+  </div>
+</section>        
+</main>
+    <footer>
+<div class="container">
+<h2 class="center"><span class="bold wf-roundedmplus1c">キャンペーン対象店舗</span></h2>
+  <div class="row">
+    <div class="col span-4">
+    <h4>倉敷沖新店</h4><br />
+    <img src="img/shop_01.jpg" alt="倉敷沖新店">
+        <p>〒710-0832</p><br />
+        <p>岡山県倉敷市田ノ上新町14-1</p><br />
+        <p>営業時間&ensp;10:00〜18:00</p><br />
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3284.743886312351!2d133.75329171522912!3d34.5853464804642!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x355157fbeecc801b%3A0xc16c804e413ffc1!2z44Ki44OR44Oe44Oz44K344On44OD44OXIOWAieaVt-ayluaWsOW6lw!5e0!3m2!1sja!2sjp!4v1615515647689!5m2!1sja!2sjp" width="100%" height="auto" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+    </div>
+    <div class="col span-4 green_back">
+    <h4>新倉敷駅前店</h4><br />
+    <img src="img/shopimg_06.jpg" alt="新倉敷駅前店">
+        <p>〒710-0253</p><br />
+        <p>岡山県倉敷市新倉敷駅前3丁目7-1</p><br />
+        <p>営業時間&ensp;10:00〜18:00</p><br />
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3285.608162663771!2d133.67842901512012!3d34.563475180470064!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x35515a59f8be4a05%3A0x2aeb68c682de9cde!2z44Ki44OR44Oe44Oz44K344On44OD44OX5paw5YCJ5pW36aeF5YmN5bqX!5e0!3m2!1sja!2sjp!4v1615515819017!5m2!1sja!2sjp" width="100%" height="auto" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+    </div>
+    <div class="col span-4">
+    <h4>倉敷水島店</h4><br />
+    <img src="img/shopimg_07.jpg" alt="倉敷水島店">
+        <p>〒712-8032</p><br />
+        <p>岡山県倉敷市北畝6丁目1-7</p><br />
+        <p>営業時間&ensp;10:00〜18:00</p><br />
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3286.542338220958!2d133.7490128151194!3d34.539821380476155!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x35515995585226c7%3A0x38ec1cb54ee8da85!2z44Ki44OR44Oe44Oz44K344On44OD44OX5YCJ5pW35rC05bO25bqX!5e0!3m2!1sja!2sjp!4v1615515772768!5m2!1sja!2sjp" width="100%" height="auto" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+    </div>
+  </div>
+</div>
+    </footer>
+    <div class="copyright">
+    <a href="https://www.chinsta.jp/" rel="noreferrer noopener" target="_blank">Copyright © YKG Horldings</a> 
+        </div>
+    <p id="pagetop"><a href="#">TOP</a></p>
+</body>
+</html>
